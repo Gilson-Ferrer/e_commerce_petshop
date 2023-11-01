f0c85a1de1d05773b529541cffde9b1838456391
Vinicius Luiz <077viniciu@gmail.com>
2023-11-01 11:07:30 -0300

    teste de mouseover dos cards

--- js/addremovbtn.js
+++ js/addremovbtn.js
@@ -1,1 +1,9 @@
-const card = document.getElementsByClassName("card");
+let cards = document.querySelectorAll(".card");
+let botaoAdicionar = document.querySelectorAll(".botao-adicionar");
+let botaoRemover = document.querySelectorAll(".botao-remover");
+
+cards.forEach(card => {
+    card.addEventListener("mouseover", () => {
+        console.log("Mouse passou por cima de um card");
+    })
+})
